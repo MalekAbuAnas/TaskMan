aa334c6c7d9e986c230db527a62807dc8c0becc9
Abu Anas <alsaieddesign@gmail.com>
2023-10-30 19:53:19 +0300

    Creating the token

--- server/server.js
+++ server/server.js
@@ -1,19 +1,62 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+app.use(cors());
+app.use(express.json());
 
 // ===== DATA BASE SETUP =====
 const
-    username = "MalekAlsayed",
-    password = "3368671636302586AbuAnas",
-    database = 'TaskMan';
+    username = process.env.USERNAME,
+    password = process.env.PASSWORD,
+    database = process.env.DATABASE;
     BASE_URL = `mongodb+srv://${username}:${password}@cluster0.aigfhce.mongodb.net/${database}?retryWrites=true&w=majority`;
-
 
 mongoose.connect(BASE_URL).then(
     console.log('Connected to Database')
 ).catch((err) => console.log(err))
+
+// ===== ROUTING =====
+
+const AdminModel = require('./models/Admins');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+
+    const admin = await AdminModel.findOne({username});
+    admin && res.json({message: "Admin already exists"});
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const newAdmin = new AdminModel({
+        username,
+        password: hashedPassword
+    });
+
+    await newAdmin.save();
+    return res.send("Admin created successfully");
+});
+
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+
+    
+    const admin = await AdminModel.findOne({username});
+    !admin && res.json({message: "Admin dosen't exist!"});
+
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+
+    !isPasswordValid && res.json({message: "Username or Password is not correct!"});
+
+    const token = jwt.sign({id: admin._id}, process.env.SECRET)
+
+    return res.json({
+        token, adminId : admin._id
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
